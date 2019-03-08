ac57a43d4ee85f0a25851687261fe42e4b04e73c
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2019-03-08 20:12:11 +0100

    fix for dev test

--- src/containers/dashboard/components/Admin/AdminConfig.jsx
+++ src/containers/dashboard/components/Admin/AdminConfig.jsx
@@ -9,22 +9,20 @@ class AdminConfig extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <AdminBar title={`Panneau d'administration`}/>
-
+                <AdminBar title={`Panneau d'administration`} />
             </React.Fragment>
         )
     }
 }
 
-const mapStateToProps = state => ({
-    display: state.admin.display
-})
+// const mapStateToProps = state => ({
+//     display: state.admin.display
+// })
 
-const mapDispatchToProps = dispatch => ({
-    fetchDisplays: () => dispatch(fetchDisplays())
-})
+// const mapDispatchToProps = dispatch => ({
+//     fetchDisplays: () => dispatch(fetchDisplays())
+// })
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(AdminConfig)
+export default connect()(AdminConfig)
+// mapStateToProps,
+// mapDispatchToProps
