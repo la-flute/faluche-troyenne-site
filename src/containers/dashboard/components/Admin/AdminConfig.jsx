b52e9df93f65a4214c8b322e925765628abc9568
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2019-03-17 12:05:33 +0100

    Managing displays

--- src/containers/dashboard/components/Admin/AdminConfig.jsx
+++ src/containers/dashboard/components/Admin/AdminConfig.jsx
@@ -1,26 +1,66 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { getDisplays, enableDisplay } from '../../../../modules/displays'
 
 import AdminBar from './AdminBar'
+import { List } from 'antd'
+import ValidDisplayRender from './components/ValidDisplayRender'
 
 class AdminConfig extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props.getDisplays()
+    }
+
+    enableDisplay = code => {
+        console.log(code)
+    }
+
+    disableDisplay = () => {
+        console.log('ne plus afficher')
+    }
+
     render() {
+        console.log(this.props)
         return (
             <React.Fragment>
                 <AdminBar title={`Panneau d'administration`} />
+                <List
+                    itemLayout='horizontal'
+                    dataSource={this.props.displays}
+                    renderItem={item => (
+                        <List.Item
+                            actions={[
+                                <ValidDisplayRender
+                                    code={item.code}
+                                    render={item.render}
+                                />,
+                            ]}
+                        >
+                            <List.Item.Meta
+                                title={`${item.code}`}
+                                description={`La seule et unique ${
+                                    item.name
+                                } créée par Fleuh et Arnaud pour amuser les trous du cul`}
+                            />
+                        </List.Item>
+                    )}
+                />
             </React.Fragment>
         )
     }
 }
 
-// const mapStateToProps = state => ({
-//     display: state.admin.display
-// })
+const mapStateToProps = state => ({
+    displays: state.displays.displays,
+})
 
-// const mapDispatchToProps = dispatch => ({
-//     fetchDisplays: () => dispatch(fetchDisplays())
-// })
+const mapDispatchToProps = dispatch => ({
+    getDisplays: () => dispatch(getDisplays()),
+    enableDisplay: code => dispatch(enableDisplay(code)),
+})
 
-export default connect()(AdminConfig)
-// mapStateToProps,
-// mapDispatchToProps
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AdminConfig)
--- src/containers/dashboard/components/Admin/AdminValid.jsx
+++ src/containers/dashboard/components/Admin/AdminValid.jsx
@@ -8,159 +8,169 @@ import ValidUser from './components/ValidUser'
 import { fetchUsers } from '../../../../modules/admin'
 
 class AdminValid extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      showPaid: true,
-      showFiche: true,
-      showAttestation: true,
-      search: ''
-    }
-
-    this.props.fetchUsers()
-  }
-
-  render() {
-    let { users } = this.props
-
-    if (!users) {
-      return <Spin />
-    }
-    users = users.filter(user => {
-      let fulltext = user.firstName + ' ' + user.lastName
-      if (user.town) fulltext += ' ' + user.town
-      if (user.nickName) fulltext += ' ' + user.nickName
-      if (user.studies) fulltext += ' ' + user.studies
-      return (
-        fulltext.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
-      )
-    })
-    // Apply filters
-    let rows = users.map(user => {
-      return { ...user, id2: user.id }
-    })
-    // Apply column filters
-    let columns = [
-      {
-        title: 'Nom',
-        dataIndex: 'lastName'
-      },
-      {
-        title: 'Prénom',
-        dataIndex: 'firstName'
-      },
-      {
-        title: 'Filière',
-        dataIndex: 'studies'
-      },
-      {
-        title: 'Ville',
-        dataIndex: 'town'
-      }
-    ]
-
-    if (this.state.showPaid) {
-      columns.push({
-        title: 'A payé',
-        dataIndex: 'paid',
-        render: paid => {
-          return paid ? (
-            <Icon type='check' style={{ color: 'green' }} />
-          ) : (
-            <Icon type='close' style={{ color: 'red' }} />
-          )
+        this.state = {
+            showPaid: true,
+            showFiche: true,
+            showAttestation: true,
+            search: '',
         }
-      })
-    }
-    if (this.state.showFiche) {
-      columns.push({
-        title: 'Fiche',
-        dataIndex: 'trajet',
-        render: trajet => {
-          return trajet ? (
-            <Icon type='check' style={{ color: 'green' }} />
-          ) : (
-            <Icon type='close' style={{ color: 'red' }} />
-          )
-        }
-      })
-    }
-    if (this.state.showAttestation) {
-      columns.push({
-        title: 'Attestation',
-        dataIndex: 'attestation',
-        render: attestation => {
-          return attestation ? (
-            <Icon type='check' style={{ color: 'green' }} />
-          ) : (
-            <Icon type='close' style={{ color: 'red' }} />
-          )
-        }
-      })
-    }
-    columns.push({
-      title: 'Actions',
-      dataIndex: 'id',
-      render: id => <ValidationListActions userId={id} users={users} />
-    })
-    columns.push({
-      title: 'Valider le dossier',
-      dataIndex: 'id2',
-      render: id => <ValidUser userId={id} users={users} />
-    })
-    return (
-      <React.Fragment>
-        <AdminBar title={`Validation des inscriptions`} />
 
-        <Card title='Affichage' style={{ marginTop: '20px' }}>
-          <Checkbox
-            checked={this.state.showPaid}
-            onChange={() => this.setState({ showPaid: !this.state.showPaid })}
-          >
-            A payé
-          </Checkbox>
-          <Checkbox
-            checked={this.state.showFiche}
-            onChange={() => this.setState({ showFiche: !this.state.showFiche })}
-          >
-            Fiche
-          </Checkbox>
-          <Checkbox
-            checked={this.state.showFiche}
-            onChange={() =>
-              this.setState({ showAttestation: !this.state.showAttestation })
-            }
-          >
-            Attestation
-          </Checkbox>
-          <Input
-            value={this.state.search}
-            onChange={e => this.setState({ search: e.target.value })}
-            placeholder='Rechercher un utilisateur par nom, prénom, surnom, fillière ou ville'
-          />
-        </Card>
-        <Table
-          columns={columns}
-          dataSource={rows}
-          rowKey='id'
-          locale={{ emptyText: 'Aucun utilisateur' }}
-          style={{ marginTop: '20px' }}
-        />
-      </React.Fragment>
-    )
-  }
+        this.props.fetchUsers()
+    }
+
+    render() {
+        let { users } = this.props
+
+        if (!users) {
+            return <Spin />
+        }
+        users = users.filter(user => {
+            let fulltext = user.firstName + ' ' + user.lastName
+            if (user.town) fulltext += ' ' + user.town
+            if (user.nickName) fulltext += ' ' + user.nickName
+            if (user.studies) fulltext += ' ' + user.studies
+            return (
+                fulltext
+                    .toLowerCase()
+                    .indexOf(this.state.search.toLowerCase()) !== -1
+            )
+        })
+        // Apply filters
+        let rows = users.map(user => {
+            return { ...user, id2: user.id }
+        })
+        // Apply column filters
+        let columns = [
+            {
+                title: 'Nom',
+                dataIndex: 'lastName',
+            },
+            {
+                title: 'Prénom',
+                dataIndex: 'firstName',
+            },
+            {
+                title: 'Filière',
+                dataIndex: 'studies',
+            },
+            {
+                title: 'Ville',
+                dataIndex: 'town',
+            },
+        ]
+
+        if (this.state.showPaid) {
+            columns.push({
+                title: 'A payé',
+                dataIndex: 'paid',
+                render: paid => {
+                    return paid ? (
+                        <Icon type='check' style={{ color: 'green' }} />
+                    ) : (
+                        <Icon type='close' style={{ color: 'red' }} />
+                    )
+                },
+            })
+        }
+        if (this.state.showFiche) {
+            columns.push({
+                title: 'Fiche',
+                dataIndex: 'trajet',
+                render: trajet => {
+                    return trajet ? (
+                        <Icon type='check' style={{ color: 'green' }} />
+                    ) : (
+                        <Icon type='close' style={{ color: 'red' }} />
+                    )
+                },
+            })
+        }
+        if (this.state.showAttestation) {
+            columns.push({
+                title: 'Attestation',
+                dataIndex: 'attestation',
+                render: attestation => {
+                    return attestation ? (
+                        <Icon type='check' style={{ color: 'green' }} />
+                    ) : (
+                        <Icon type='close' style={{ color: 'red' }} />
+                    )
+                },
+            })
+        }
+        columns.push({
+            title: 'Actions',
+            dataIndex: 'id',
+            render: id => <ValidationListActions userId={id} users={users} />,
+        })
+        columns.push({
+            title: 'Valider le dossier',
+            dataIndex: 'id2',
+            render: id => <ValidUser userId={id} users={users} />,
+        })
+        return (
+            <React.Fragment>
+                <AdminBar title={`Validation des inscriptions`} />
+
+                <Card title='Affichage' style={{ marginTop: '20px' }}>
+                    <Checkbox
+                        checked={this.state.showPaid}
+                        onChange={() =>
+                            this.setState({ showPaid: !this.state.showPaid })
+                        }
+                    >
+                        A payé
+                    </Checkbox>
+                    <Checkbox
+                        checked={this.state.showFiche}
+                        onChange={() =>
+                            this.setState({ showFiche: !this.state.showFiche })
+                        }
+                    >
+                        Fiche
+                    </Checkbox>
+                    <Checkbox
+                        checked={this.state.showFiche}
+                        onChange={() =>
+                            this.setState({
+                                showAttestation: !this.state.showAttestation,
+                            })
+                        }
+                    >
+                        Attestation
+                    </Checkbox>
+                    <Input
+                        value={this.state.search}
+                        onChange={e =>
+                            this.setState({ search: e.target.value })
+                        }
+                        placeholder='Rechercher un utilisateur par nom, prénom, surnom, fillière ou ville'
+                    />
+                </Card>
+                <Table
+                    columns={columns}
+                    dataSource={rows}
+                    rowKey='id'
+                    locale={{ emptyText: 'Aucun utilisateur' }}
+                    style={{ marginTop: '20px' }}
+                />
+            </React.Fragment>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
-  users: state.admin.users
+    users: state.admin.users,
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchUsers: () => dispatch(fetchUsers())
+    fetchUsers: () => dispatch(fetchUsers()),
 })
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(AdminValid)
