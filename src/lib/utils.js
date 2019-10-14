d8f24ff6a647a9ca406df1920696e98ef19bd0b4
François-Luc Haghenbeek <francois-luchaghenbeek@MacBook-Pro-de-Francois-Luc.local>
2019-10-14 11:09:02 +0200

    Update error handler.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,8 +1,8 @@
 import { actions as notifActions } from 'redux-notifications'
 
-export const handleAPIerror = (dispatch, error, kind) => {
+export const handleAPIerror = (dispatch, error) => {
     console.log(error)
-    dispatch(notification(error.response.data.error, kind))
+    dispatch(notification(error.response.data.error, 'danger'))
 }
 
 export const notification = (message, kind) => {
