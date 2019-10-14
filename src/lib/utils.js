53f271e1c5608d9290698b82990c93bb127f3536
François-Luc Haghenbeek <francois-luchaghenbeek@MacBook-Pro-de-Francois-Luc.local>
2019-10-14 10:20:27 +0200

    Improve error handler.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,9 @@
-export const handleAPIerror = (error) => {
+export const handleAPIerror = (error, kind) => {
     console.log(error)
             dispatch(
                 notifActions.notifSend({
                     message: errorToString(error.response.data.error),
-                    kind: 'danger',
+                    kind: kind,
                     dismissAfter: 2000,
                 })
             )
